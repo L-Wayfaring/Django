6517f6b06e68506d194cc4edc378d2eac520cbcf
Mohammed Ashraf <tanviralam758@gmail.com>
2024-11-02 23:11:32 +0600

    BookStore

---
+++ BookStore/book/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class books(models.Model):
+    title = models.CharField(max_length=200)
+    published_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
